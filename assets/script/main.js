d6bcc59bd38ad80c9fda7079fd1db4b07587f141
morita176 <githubmorita83@gmail.com>
2026-03-24 00:23:03 +0900

    20260324修正

---
+++ assets/script/main.js
@@ -0,0 +1,24 @@
+// Slickリファレンス
+// https://kenwheeler.github.io/slick/
+
+
+$('.works-carousel').slick({
+  autoplay: true, // 自動再生
+  autoplaySpeed: 2000, // 自動再生の速さ、単位はミリ秒
+  arrows: true, // 左右のナビゲーションを有効
+  dots: true, // ドットを表示
+  centerMode: true,
+  centerPadding: "200px",
+  slidesToShow: 1,
+  pauseOnHover: false,
+
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        centerPadding: '40px'
+      }
+    }
+  ]
+});
+
